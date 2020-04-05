a19bf5e905b7391ec5e2952ec5e352c33c2f72f1
Jacob Lindgren <jlindgre@redhat.com>
2020-04-05 14:15:17 -0500

    speed up response of import

--- app/controllers/concerns/bulk_uploadable.rb
+++ app/controllers/concerns/bulk_uploadable.rb
@@ -6,14 +6,13 @@ module BulkUploadable
   included do
     def bulk_upload
       file = File.read(params.require(:file))
-      model = controller_name.classify.constantize
 
-      # clear the current records
+      # clear the current records without callbacks
       model.delete_all
 
       model.transaction do
         model.bulk_import(file)
-        render json: model.all, status: 201
+        render json: { "imported": model.count }, status: 201
       end
     rescue StandardError => e
       render json: { "error": e.message }, status: 400
