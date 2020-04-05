1ce892a1ede7dda563d966a6f84ad5f12ac5e763
Jacob Lindgren <jlindgre@redhat.com>
2020-04-05 18:08:04 -0500

    postgres change

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_04_05_172031) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "counties", force: :cascade do |t|
     t.date "date"
     t.string "name"
